29257fadca87407eb298a62aef36d4904afd5b56
Sandro <thesandro1998@gmail.com>
2020-07-03 14:29:41 +0400

    Permission check added

--- app/src/main/java/com/possystem/posapp/MainActivity.kt
+++ app/src/main/java/com/possystem/posapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.possystem.posapp
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.possystem.posapp.ui.adapters.BottomNavigationViewPagerAdapter
@@ -19,6 +24,12 @@ class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //temp camera permission
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+        }
+        else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),1)
+        }
         init()
     }
     private fun init(){
